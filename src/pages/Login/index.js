a840f1b789351e74e6a053918539a51a10c3bc15
ChendyWS <chendywidyasanti@gmail.com>
2021-11-01 17:27:03 +0800

    second commit

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { LoginBg } from '../../assets/image';
+import { Input, Button, Gap, Link } from '../../components/atoms';
 
 const Login = () => {
     return (
-        <div>
-            <p>Login Page</p>
+        <div className="main-page">
+            <div className="left">
+                <img src={LoginBg} className="bg-image" alt="imageBg"/>
+            </div>
+            <div className="right">
+                <p className="title">Login</p>
+                <Input label="Email" placeholder="Email" />
+                <Gap height={18}/>
+                <Input label="Password" placeholder="Password"/>
+                <Gap height={50} />
+                <Button title="Login" />
+                <Gap height={100} />
+                <Link title="Belum punya akun, silahkan daftar" />
+            </div>
+            
         </div>
+        
     )
 }
 
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { RegisterBg } from '../../assets/image';
 import './register.scss';
-import { Input } from '../../components/atoms';
+import { RegisterBg } from '../../assets/image';
+import { Input, Button, Gap, Link } from '../../components/atoms';
 
 
 
@@ -14,8 +14,14 @@ const Register = () => {
             <div className="right">
                 <p className="title">Register</p>
                 <Input label="Full Name" placeholder="Full Name"/>
+                <Gap height={18}/>
                 <Input label="Email" placeholder="Email" />
+                <Gap height={18}/>
                 <Input label="Password" placeholder="Password"/>
+                <Gap height={50} />
+                <Button title="Register" />
+                <Gap height={100} />
+                <Link title="Kembali ke Login" />
             </div>
             
         </div>
