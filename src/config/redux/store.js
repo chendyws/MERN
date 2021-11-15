4409f6b07d66627ce36cb57bc0a340993bdbc38d
ChendyWS <chendywidyasanti@gmail.com>
2021-11-15 17:25:33 +0800

    Change Value from State Global Redux

--- src/config/redux/store.js
+++ src/config/redux/store.js
@@ -1,12 +1,23 @@
 const { createStore } = require("redux");
 
 const initialState = {
-  dataBlog: [],
+  dataBlogs: [],
   name: 'Chendy'
 }
 
 const reducer = (state = initialState, action) => {
-  
+  if(action.type === 'UPDATE_DATA_BLOG') {
+    return {
+      ...state,
+      dataBlogs: action.value.payload
+    }
+  }
+  if(action.type === 'UPDATE_NAME') {
+    return {
+      ...state,
+      name: 'Widya'
+    }
+  }
   return state;
 }
 
